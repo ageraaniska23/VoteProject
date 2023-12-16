090e782a87e02161871b56bf74e3a65dba76c7dc
Agera Aniska <agraaniska@gmail.com>
2023-12-16 12:31:41 +0700

    admin user dash

--- fe/src/page/Admin/Admin.jsx
+++ fe/src/page/Admin/Admin.jsx
@@ -1,17 +1,218 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Admin = () => {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setModalOpen(!isModalOpen);
+  };
+
+  const handleAddProduct = () => {
+    // Handle the logic for adding a new product
+    // You can add API calls or any other logic here
+    // For now, let's just close the modal
+    toggleModal();
+  };
+
   return (
     <div className="bg-blue-950 h-screen">
       <div className="grid grid-rows-3 grid-flow-col gap-4 mt-14">
         <div className="row-span-3 ...">
-          01
+          <div className="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden mt-24">
+            <div className="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
+              <div className="w-full md:w-1/2">
+                <form className="flex items-center">
+                  <label htmlFor="simple-search" className="sr-only">
+                    Search
+                  </label>
+                  <div className="relative w-full">
+                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                      <svg
+                        aria-hidden="true"
+                        className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        {/* ... existing code ... */}
+                      </svg>
+                    </div>
+                    <input
+                      type="text"
+                      id="simple-search"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                      placeholder="Search"
+                      required
+                    />
+                  </div>
+                  <button
+                    className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 text-center ml-4 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    type="button"
+                    onClick={toggleModal}
+                  >
+                    Add
+                  </button>
+                </form>
+              </div>
+            </div>
+            <div className="overflow-x-auto">
+              <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                  <tr>
+                    <th scope="col" className="px-4 py-3">
+                      Address
+                    </th>
+                    <th scope="col" className="px-4 py-3">
+                      Nik
+                    </th>
+                    <th scope="col" className="px-4 py-4">
+                      Ussenmae
+                    </th>
+                    <th scope="col" className="px-4 py-3">
+                      Email
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr className="border-b dark:border-gray-700">
+                    {/* ... existing code ... */}
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
         <div className="col-span-2 ...">02</div>
         <div className="row-span-2 col-span-2 ...">03</div>
       </div>
+
+      {/* Modal toggle */}
+      {isModalOpen && (
+  <div
+    id="crud-modal"
+    tabIndex="-1"
+    aria-hidden="true"
+    className="fixed top-0 right-0 left-0 bottom-0 z-50 flex justify-center items-center bg-gray-800 bg-opacity-50"
+  >
+    {/* Main modal */}
+    <div className="relative p-4 w-full max-w-md max-h-full">
+      {/* Modal content */}
+      <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+        {/* Modal header */}
+        <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+          <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+            Input Pengguna Baru
+          </h3>
+          <button
+            type="button"
+            className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+            data-modal-toggle="crud-modal"
+            onClick={toggleModal}
+          >
+            <svg
+              className="w-3 h-3"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 14"
+            >
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+            </svg>
+            <span className="sr-only">Close modal</span>
+          </button>
+        </div>
+        {/* Modal body */}
+        <form className="p-4 md:p-5">
+          <div className="grid gap-4 mb-4 grid-cols-2">
+            <div className="col-span-2">
+              <label htmlFor="Ussername" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Ussername
+              </label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Input Ussername"
+                required=""
+              />
+            </div>
+            <div className="col-span-2">
+              <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Password
+              </label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="password"
+                required=""
+              />
+            </div>
+            <div className="col-span-2 sm:col-span-1">
+              <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Email
+              </label>
+              <input
+                type="Email"
+                name="email"
+                id="email"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Email"
+                required=""
+              />
+            </div>
+            <div className="col-span-2 sm:col-span-1">
+              <label htmlFor="WalletAdress" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Wallet Address
+              </label>
+              <input
+                type="address"
+                name="address"
+                id="address"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="wallet Address"
+                required=""
+              />
+            </div>
+            <div className="col-span-2">
+              <label htmlFor="nik" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                NIK
+              </label>
+              <input
+                type="number"
+                name="Nik"
+                id="Nik"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Nik"
+                required=""
+              />
+          
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            onClick={handleAddProduct}
+          >
+            <svg
+              className="me-1 -ms-1 w-5 h-5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path fillRule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clipRule="evenodd"></path>
+            </svg>
+            Add New Usser
+          </button>
+        </form>
+      </div>
     </div>
-  );  
+  </div>
+)}
+    </div>
+  );
 };
 
 export default Admin;
