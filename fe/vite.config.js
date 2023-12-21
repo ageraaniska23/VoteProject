aff60779e4cda4e45e2762eea2cb466240b30182
Agera Aniska <agraaniska@gmail.com>
2023-12-21 17:08:50 +0700

    fix eror

--- fe/vite.config.js
+++ fe/vite.config.js
@@ -1,8 +1,8 @@
-import ReactRefreshPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ReactRefreshPlugin()],
+  plugins: [react()],
 });
